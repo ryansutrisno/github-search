23300ad5c8e9889e8c08c80a3d9a2e6e587142e1
Ryan Sutrisno <ryan.sutrisno@gmail.com>
2023-09-21 23:21:35 +0700

    added footer

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Container } from "@chakra-ui/react";
 import Navbar from "../Navbar";
+import Footer from "../Footer";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
-      <Container p="5" minW="80%" minH="100vh">
+      <Container p="5" minW="80%" minH="70vh">
         {children}
       </Container>
+      <Footer />
     </>
   );
 }
